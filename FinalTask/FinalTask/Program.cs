fe5799bb70766d402d86c3b79943de1c7070f3e8
viktornazilin <104361827+viktornazilin@users.noreply.github.com>
2022-08-04 12:01:47 +0300

    Program is ready

---
+++ FinalTask/FinalTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FinalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Cities = new string[5] { "Bonn", "Moscow", "NY", "LA", "Rom" };
+            string[] CitiesShort = new string[Cities.Length];
+
+            ShortCitiesArray(Cities, CitiesShort);
+            PrintArray(CitiesShort);
+
+        }
+
+        static void ShortCitiesArray(string[] array1, string[] array2)
+        {
+            int count = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i].Length <= 3)
+                {
+                    array2[count] = array1[i];
+                    count++;
+                }
+            }
+        }
+        static void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
